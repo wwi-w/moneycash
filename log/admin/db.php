4eb11c1fe5f82ca1ed013274a6f9585b9ebb12e9
wwi-w <77251113+wwi-w@users.noreply.github.com>
2021-03-18 01:25:58 +0100

    Update db.php

--- log/admin/db.php
+++ log/admin/db.php
@@ -3,7 +3,7 @@
 $username = "adeniran1234";
 $password = "JOShua123@#";
 $database = "users";
-$host="mysql-23766-0.cloudclusters.net";
+$host="mysql-23766-0.cloudclusters.net:23808";
 $db = mysqli_connect($host,$username,$password,$database);
 
 if(!$db)
